57864ba910a73dd9ae216aea91e1a77cc272eaa4
auleki <kodagiwa@gmail.com>
2020-06-26 07:46:18 +0100

    footer adjustment

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const morgan = require('morgan'); 
 const cors = require('cors');
 const path = require('path');
+// const con
+
 
 const app = express(); 
 const PORT = process.env.PORT || 8000;
 
 const apiRoutes = require('./routes/api');
+
 
 const url = 'mongodb+srv://admin:admin12345@mern-deploy-02fwd.mongodb.net/test?retryWrites=true&w=majority'
 const options = {
@@ -17,7 +20,15 @@ const options = {
 
 // const PodcastModel = require('./models/Podcast');
 
-mongoose.connect(process.env.MONGODB_URI || url, options )
+mongoose.connect(process.env.MONGODB_URI  || url, options )
+
+// shontan lagbaja
+// fuck the society
+// prefix
+// Ellen Johnson Sirleaf 2006
+// women are not sidelined
+// they are not looked down upon
+// governed by men
 
 
 mongoose.connection.on('connected', () => {
