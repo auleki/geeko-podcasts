a97db705eae99f24ec06cb7ef593593453e5a508
auleki <kodagiwa@gmail.com>
2020-06-26 08:13:47 +0100

    lol shh

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan'); 
 const cors = require('cors');
 const path = require('path');
-// const con
+require('dotenv').config();
 
 
 const app = express(); 
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 const apiRoutes = require('./routes/api');
 
 
-const url = 'mongodb+srv://admin:admin12345@mern-deploy-02fwd.mongodb.net/test?retryWrites=true&w=majority'
+// const url = 'mongodb+srv://admin:admin12345@mern-deploy-02fwd.mongodb.net/test?retryWrites=true&w=majority'
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -20,7 +20,7 @@ const options = {
 
 // const PodcastModel = require('./models/Podcast');
 
-mongoose.connect(process.env.MONGODB_URI  || url, options )
+mongoose.connect(process.env.MONGODB_URI, options )
 
 // shontan lagbaja
 // fuck the society
@@ -28,7 +28,8 @@ mongoose.connect(process.env.MONGODB_URI  || url, options )
 // Ellen Johnson Sirleaf 2006
 // women are not sidelined
 // they are not looked down upon
-// governed by men
+// there are MRS who take pride in their name
+// society is not governed by men
 
 
 mongoose.connection.on('connected', () => {
